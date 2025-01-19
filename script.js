2c37bf801d4925210e61445b2434732d6e3c91d6
vipin018 <vipinpathak018@gmail.com>
2025-01-19 13:13:12 +0530

    added smooth scroll

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true,
+    lerp: 0.07, // lerp is the amount of time it takes to scroll to the next page
+    
 });
+
+// the upper code is to make the page scroll smoothly the working of this is in the index.html file where the id of the main div is #main and the smooth: true is to make the page scroll smoothly and the el: document.querySelector('#main') is to select the main div 
